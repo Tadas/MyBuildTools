92a322bb0936a75deacf8f9b27aa436ed5ceb71b
Tadas <thetadas+github@gmail.com>
2018-04-14 11:44:26 +0100

    Initial commit

---
+++ BuildTools/Tests/BuildTools.Tests.ps1
@@ -0,0 +1,259 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.ps1', '.psd1'
+Import-Module "$here\..\$sut" -Force
+
+Describe "BuildTools" {
+	BeforeEach {
+		$TwoComponentLastVersion = [Version]"1.12"
+		$LastVersion = [Version]"2.3.4.5"
+	}
+
+	Context "Hint parsing" {
+		It "accepts no leading space 'blah+semver: minor'" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four+semver: minor",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.4.0.0"
+		}
+
+		It "accepts no middle space 'blah +semver:minor'" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver:minor",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.4.0.0"
+		}
+
+		It "accepts no trailing space 'blah +semver: minorblah'" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: minorblahblah",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.4.0.0"
+		}
+
+		It "unknown hints gets ignored" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one +semver: fexfix",
+					"Commit two +semver+semver:",
+					"Commit three +semver: +semver:",
+					"Commit four+semver: manor",
+					"Commit five+semver: mansion"
+				)
+			$NewVersion | Should -Be "2.3.5.0"
+		}
+	}
+
+	Context "Short version numbers" {
+		It "expands a two component version" {
+			$NewVersion = Bump-Version -StartingVersion $TwoComponentLastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four",
+					"Commit five"
+				)
+				$NewVersion | Should -Be "1.12.1"
+			
+		}
+
+		It "increments a two component version without expanding" {
+			$NewVersion = Bump-Version -StartingVersion $TwoComponentLastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two +semver: minor",
+					"Commit three",
+					"Commit four",
+					"Commit five"
+				)
+				$NewVersion | Should -Be "1.13"
+			
+		}
+	}
+
+
+	Context "Patch" {
+		It "increments patch without any hints" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.3.5.0"
+		}
+
+		It "increments patch once with a single 'patch' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three +semver: patch",
+					"Commit four",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.3.5.0"
+		}
+
+		It "increments patch once with an alternative 'fix' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three +semver: fix",
+					"Commit four",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.3.5.0"
+		}
+
+		It "increments patch once with multiple 'patch' hints" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one +semver: patch",
+					"Commit two",
+					"Commit three +semver: patch",
+					"Commit four",
+					"Commit five +semver: patch"
+				)
+			$NewVersion | Should -Be "2.3.5.0"
+		}
+	}
+
+
+
+	Context "Minor" {
+		It "increments minor once with a single 'minor' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: minor",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.4.0.0"
+		}
+
+		It "increments minor once with an alternative 'feature' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: feature",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.4.0.0"
+		}
+
+		It "increments minor once with multiple 'minor' hints" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two +semver: minor",
+					"Commit three",
+					"Commit four",
+					"Commit five +semver: minor"
+				)
+			$NewVersion | Should -Be "2.4.0.0"
+		}
+	}
+
+
+
+	Context "Major" {
+		It "increments major once with a single 'major' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: major",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "3.0.0.0"
+		}
+
+		It "increments major once with a 'breaking' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: breaking",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "3.0.0.0"
+		}
+
+		It "increments major once with multiple 'major' hints" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: major",
+					"Commit five +semver: major"
+				)
+			$NewVersion | Should -Be "3.0.0.0"
+		}
+	}
+
+
+
+	Context "Hint supersedence" {
+		It "'major' supersedes 'patch' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: major",
+					"Commit five +semver: patch"
+				)
+			$NewVersion | Should -Be "3.0.0.0"
+		}
+
+		It "'major' supersedes 'minor' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one +semver: major",
+					"Commit two",
+					"Commit three",
+					"Commit four +semver: minor",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "3.0.0.0"
+		}
+
+		It "'minor' supersedes 'patch' hint" {
+			$NewVersion = Bump-Version -StartingVersion $LastVersion `
+				-CommitMessages @(
+					"Commit one",
+					"Commit two +semver: minor",
+					"Commit three +semver: patch",
+					"Commit four",
+					"Commit five"
+				)
+			$NewVersion | Should -Be "2.4.0.0"
+		}
+	}
+}
---
+++ MyBuildTools.build.ps1
@@ -0,0 +1,114 @@
+﻿Import-Module $BuildRoot\BuildTools -Force
+
+$ProjectName      = ($BuildRoot -split '\\')[-1]
+$ArtifactPath     = "$BuildRoot\Artifacts"
+$ArtifactFullPath = "$ArtifactPath\$ProjectName.zip"
+
+task . InstallDependencies, <# Analyze, #> Test, Clean, ComputeVersionInfo, SetVersionInManifest, Build
+
+task InstallDependencies {
+	Install-Module Pester -Scope CurrentUser
+	# Install-Module PSScriptAnalyzer -Scope CurrentUser
+}
+
+# task Analyze {
+# 	$scriptAnalyzerParams = @{
+# 		Path = "$BuildRoot\"
+# 		Severity = @('Error', 'Warning')
+# 		Recurse = $true
+# 		Verbose = $false
+# 		ExcludeRule = 'PSAvoidUsingWriteHost'
+# 	}
+	
+# 	$Results = Invoke-ScriptAnalyzer @scriptAnalyzerParams
+
+# 	if ($Results) {
+# 		$Results | Format-Table
+# 		throw "One or more PSScriptAnalyzer errors/warnings where found."
+# 	}
+# }
+
+task Test {
+	$invokePesterParams = @{
+		Path = '.\Tests\*'
+		Strict = $true
+		PassThru = $true
+		Verbose = $false
+		EnableExit = $false
+	}
+
+	# Publish Test Results as NUnitXml
+	$testResults = Invoke-Pester @invokePesterParams;
+
+	$numberFails = $testResults.FailedCount
+	assert($numberFails -eq 0) ('Failed "{0}" unit tests.' -f $numberFails)
+}
+
+task ComputeVersionInfo {
+	$LastVersion = Get-LastVersionByTag
+	$LatestCommitMessages = Get-CommitsSinceVersionTag $LastVersion
+
+	$script:NewVersion = Bump-Version -StartingVersion $LastVersion -CommitMessages $LatestCommitMessages
+	$script:NewReleaseNotes = ""
+}
+
+task SetVersionInManifest ComputeVersionInfo,{
+	$ManifestFile = "$BuildRoot\$ProjectName.psd1"
+
+	(Get-Content $ManifestFile) `
+		-replace "ModuleVersion = .*", "ModuleVersion = '$NewVersion'" |
+	Out-File -FilePath $ManifestFile -Encoding utf8
+}
+
+task Clean {
+	if (Test-Path -Path $ArtifactPath) {
+		Remove-Item "$ArtifactPath/*" -Recurse -Force
+	}
+	New-Item -ItemType Directory -Path $ArtifactPath -Force | Out-Null
+}
+
+# Builds an artifact into the artifact folder
+task Build {
+	try {
+		$TempPath = New-TemporaryFolder
+
+		Get-ChildItem -File -Recurse $BuildRoot | Where-Object {
+			(-not $_.FullName.Contains("\.vscode\")) -and
+			(-not $_.FullName.Contains("\.git")) -and
+			(-not $_.FullName.Contains("\Artifacts\")) -and
+			(-not $_.FullName.Contains("\BuildTools\")) -and
+			(-not $_.FullName.Contains("\Tests\")) -and
+			(-not $_.FullName.EndsWith(".build.ps1"))
+
+		} | ForEach-Object {
+			$DestinationPath = [System.IO.Path]::Combine(
+				$TempPath,
+				$_.FullName.Substring($BuildRoot.Length + 1)
+			)
+			Write-Host "`tMoving $($_.FullName)`r`n`t`t to $DestinationPath`r`n"
+
+			# Makes sure the path is available
+			New-Item -ItemType File -Path $DestinationPath -Force | Out-Null
+			Copy-Item -LiteralPath $_.FullName -Destination $DestinationPath -Force
+		}
+		Compress-Archive -Path "$TempPath\*" -DestinationPath "$ArtifactPath\$ProjectName.zip" -Verbose -Force
+
+	} finally {
+		if(Test-Path -PathType Container -LiteralPath $TempPath) { Remove-Item -Recurse $TempPath -Force }
+	}
+}
+
+task CreateGithubReleaseAndUpload {
+	$LastVersion = Get-LastVersionByTag
+	$LatestCommitMessages = Get-CommitsSinceVersionTag $LastVersion
+	$NewVersion = Bump-Version -StartingVersion $LastVersion -CommitMessages $LatestCommitMessages
+
+	New-GithubRelease `
+		-Uri          "https://api.github.com/repos/Tadas/$ProjectName/releases" `
+		-NewVersion   $NewVersion `
+		-ReleaseNotes $NewReleaseNotes `
+		-Draft        $true `
+		-PreRelease   $false `
+		-ArtifactPath $ArtifactFullPath
+
+}
